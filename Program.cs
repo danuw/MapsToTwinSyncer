7aa17563e5ffb748205736679d095e927060b718
Dan Benitah <dan@local.com>
2021-01-10 23:26:46 +0000

    code to lookup digital twin with same id and update with map info

--- Program.cs
+++ Program.cs
@@ -4,26 +4,82 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Collections.Generic;
+using Azure.DigitalTwins.Core;
+using Azure.Identity;
+using Azure;
+//using System.Text.Json;
+
+using Microsoft.Identity.Client;
+//using Microsoft.Rest;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+using Azure.DigitalTwins.Core.Serialization;
+//using Azure.DigitalTwins.Core.Models;
+using System.Runtime.InteropServices;
 
 namespace MapsToTwinSyncer
 {
     class Program
     {
+        private static string adtInstanceUrl;
+        private static string datasetId;
+        private static string subkey;
+
+        const string adtAppId = "https://digitaltwins.azure.net";
+
+        
+        private static DigitalTwinsClient adtclient;
+
         static async Task Main(string[] args)
         {
             var httpClient = new HttpClient();
             Console.WriteLine("Hello World!");
             // Start time
             var start = DateTime.Now;
-            var datasetId = "8455dd7a-efaf-65ff-15da-3d96566369b4";
-            var subkey = "s6JTKf5gnGdgvv6iDNKon-KAnR191AMUgaIv3FMFrVg";
+
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                int width = Math.Min(Console.LargestWindowWidth, 150);
+                int height = Math.Min(Console.LargestWindowHeight, 40);
+                Console.SetWindowSize(width, height);
+            }
+            if(args!=null){
+                Console.WriteLine("You sent some pre commands as arguments...");
+            }
+            try
+            {
+                // Read configuration data from the 
+                IConfiguration config = new ConfigurationBuilder()
+                    .AddJsonFile("serviceConfig.json", true, true)
+                    .Build();
+                datasetId = config["datasetId"];
+                subkey = config["subkey"];
+                adtInstanceUrl = config["instanceUrl"];
+                Log.Ok($"Will connect to {adtInstanceUrl}");
+            } catch (Exception e)
+            {
+                Log.Error($"Could not read service configuration file serviceConfig.json");
+                Log.Alert($"Please copy serviceConfig.json.TEMPLATE to serviceConfig.json");
+                Log.Alert($"and edit to reflect your service connection settings");
+                Environment.Exit(0);
+            }
+            
             var subquery = $"&subscription-key={subkey}";
+            Log.Ok("Authenticating...");
+
+            var credential = new DefaultAzureCredential();
+            adtclient = new DigitalTwinsClient(new Uri(adtInstanceUrl), credential);
+            Console.WriteLine($"Service client created – ready to go");
 
             //https://us.atlas.microsoft.com/wfs/datasets/8455dd7a-efaf-65ff-15da-3d96566369b4/collections/unit/items?api-version=1.0&subscription-key=---------------------
             var postfeatureUpdateUrlTemplate = $"https://us.atlas.microsoft.com/wfs/datasets/{datasetId}/collections/unit/items?api-version=1.0";
 
             // retreive unit info
-            var result = await ProcessFeatureCollection(httpClient, postfeatureUpdateUrlTemplate, subquery);
+            var result = await ProcessFeatureCollection(httpClient, adtclient, postfeatureUpdateUrlTemplate, subquery);
 
             // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
 
@@ -37,18 +93,18 @@ namespace MapsToTwinSyncer
         }
 
 
-        private static async Task<int> ProcessFeatureCollection(HttpClient httpClient, string url, string subquery)
+        private static async Task<int> ProcessFeatureCollection(HttpClient httpClient, DigitalTwinsClient adtclient, string url, string subquery)
         {
             int total= 0;
             var result = await httpClient.GetAsync(url+subquery);
-            Console.WriteLine(result.StatusCode.ToString());
+            //Console.WriteLine(result.StatusCode.ToString());
             if (!result.IsSuccessStatusCode)
             {
                 Console.WriteLine("ERROR posting changes to Azure Maps");
             }
             else{
                 Root col = JsonConvert.DeserializeObject<Root>(result.Content.ReadAsStringAsync().Result); 
-                Console.WriteLine($"{col.type} {col.numberReturned.ToString()}");
+                //Console.WriteLine($"{col.type} {col.numberReturned.ToString()}");
                 total = col.numberReturned;
                 foreach (var feature in col.features)
                 {
@@ -56,21 +112,34 @@ namespace MapsToTwinSyncer
 
                     var firstPoint = feature.geometry.coordinates[0][0];// TODO - get the center point
                     // use first 
-                    Console.WriteLine($"{feature.properties.name} as {feature.id}, [{firstPoint[0]},{firstPoint[1]}] on {feature.properties.levelId}]");
+                    //Console.WriteLine($"{feature.properties.name} as {feature.id}, [{firstPoint[0]},{firstPoint[1]}] on {feature.properties.levelId}]");
+                    var mapcenter = $"[{firstPoint[0]},{firstPoint[1]}]";
+                    Console.WriteLine($"Room\t{feature.properties.name}\t{feature.properties.levelId}\tcontains\t\t{feature.id}\t{mapcenter}");
 
+                    // Run a query for all twins   
+                    string query = $"SELECT * FROM digitaltwins T WHERE T.$dtId = '{feature.properties.name}'";
+                    AsyncPageable<BasicDigitalTwin> results = adtclient.QueryAsync<BasicDigitalTwin>(query);
+
+                    await foreach (BasicDigitalTwin twin in results)
+                    {
+                        Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(twin));
+                        Console.WriteLine("---------------");
+                        // Update   Update 
+                        await UpdateTwinProperty(adtclient, feature.properties.name, "replace", "/MapCenter", mapcenter);
+                    }
 
                 }
-return total;// TODO enable all results
+//return total;// TODO enable all results
                 // continue if there is a next link
                 foreach (var link in col.links)
                 {
                     if(link.rel == "next")
                     {
                         var next = link.href;
-                        Console.WriteLine(next);
+                        //Console.WriteLine(next);
                         if(!string.IsNullOrWhiteSpace(next))
                         {
-                            total += await ProcessFeatureCollection(httpClient, next.Replace("https://atlas.microsoft.com", "https://us.atlas.microsoft.com"), subquery);// making sure we connect to the US server
+                            total += await ProcessFeatureCollection(httpClient, adtclient, next.Replace("https://atlas.microsoft.com", "https://us.atlas.microsoft.com"), subquery);// making sure we connect to the US server
                         }
                     }
                 }
@@ -78,6 +147,22 @@ return total;// TODO enable all results
 
             return total;
         }
+
+        public static async Task UpdateTwinProperty(DigitalTwinsClient client, string twinId, string operation, string propertyPath, string val)
+        {
+            // Update twin property
+            try
+            {
+                var updateTwinData = new JsonPatchDocument();
+                updateTwinData.AppendReplace(propertyPath, val);
+                await client.UpdateDigitalTwinAsync(twinId, updateTwinData);
+            }
+            catch (RequestFailedException exc)
+            {
+                Log.Error($"*** Error:{exc.Status}/{exc.Message}");
+            }
+        }
+    
     }
     // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
     public class Geometry    {
---
+++ readme.md
@@ -0,0 +1,11 @@
+
+mkdir myapp
+dotnet new console
+dotnet add package Newtonsoft.Json
+
+dotnet add package Azure.DigitalTwins.Core
+
+dotnet add package Azure.Identity
+
+dotnet add package Microsoft.Extensions.Configuration
+dotnet add package Microsoft.Extensions.Configuration.Json
